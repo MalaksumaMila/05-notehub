a0892c3f2eb80f20bdfb0870f69087b27bc5d574
Mila <malaksumaaa@gmail.com>
2025-11-22 18:11:57 +0100

    add Note List

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import NoteList from '../NoteList/NoteList';
+import fetchNotes from '../../services/noteService';
 import css from './App.module.css';
 import ReactPaginate from 'react-paginate';
 import { useQuery, keepPreviousData } from '@tanstack/react-query';
@@ -8,10 +10,26 @@ import type { Note } from '../../types/note';
 import { Toaster } from 'react-hot-toast';
 import toast from 'react-hot-toast';
 
-<div className={css.app}>
-  <header className={css.toolbar}>
-    {/* Компонент SearchBox */}
-    {/* Пагінація */}
-    {/* Кнопка створення нотатки */}
-  </header>
-</div>;
+export default function App() {
+  const query = '';
+  const page = 1;
+  const sortOrder = 'created';
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['notes', query, page, sortOrder],
+    queryFn: () => fetchNotes(query, page, sortOrder),
+  });
+
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        {isLoading && <p>Loading...</p>}
+        {isError && <p>Error...</p>}
+        {data && !isLoading && <NoteList notes={data.notes} />}
+        {/* Компонент SearchBox */}
+        {/* Пагінація */}
+        {/* Кнопка створення нотатки */}
+      </header>
+    </div>
+  );
+}
