63e20292096abc67ace55059336a12f67848bba2
Mila <malaksumaaa@gmail.com>
2025-11-24 12:28:39 +0100

    mistake

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -6,23 +6,22 @@ interface PaginationProps {
   setPage: (page: number) => void;
   pageCount: number;
 }
-export default function Pagination ({ page, setPage, pageCount }: PaginationProps) {
-
-
-    return (
-         <ReactPaginate
-            pageCount={pageCount}
-            pageRangeDisplayed={5}
-            marginPagesDisplayed={1}
-            onPageChange={({ selected }) => setPage(selected + 1)}
-            forcePage={page - 1}
-            containerClassName={css.pagination}
-            activeClassName={css.active}
-            nextLabel="→"
-            previousLabel="←"
-          />
-
-    )
-
+export default function Pagination({
+  page,
+  setPage,
+  pageCount,
+}: PaginationProps) {
+  return (
+    <ReactPaginate
+      pageCount={pageCount}
+      pageRangeDisplayed={5}
+      marginPagesDisplayed={1}
+      onPageChange={({ selected }) => setPage(selected + 1)}
+      forcePage={page - 1}
+      containerClassName={css.pagination}
+      activeClassName={css.active}
+      nextLabel="→"
+      previousLabel="←"
+    />
+  );
 }
- GET https://notehub-public.goit.study/api/notes?page=1&perPage=12
