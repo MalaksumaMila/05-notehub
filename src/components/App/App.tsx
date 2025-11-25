8b434c59cd9ca5b98b994ce1f5b8a6a071c80807
Mila <malaksumaaa@gmail.com>
2025-11-25 23:06:31 +0100

    corrector

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import NoteList from '../NoteList/NoteList';
-import fetchNotes from '../../services/noteService';
+import { fetchNotes } from '../../services/noteService';
 import css from './App.module.css';
 import Pagination from '../Pagination/Pagination';
 import { useQuery, keepPreviousData } from '@tanstack/react-query';
